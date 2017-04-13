3dba7cf6b61dc7dc368f371c524879a52e24a135
cohagen <stefanrothermich@gmail.com>
2017-04-13 01:57:27 -0500

    added bootstrap validation but need to fix dynamic checkboxes

--- functions/announcementCreation.js
+++ functions/announcementCreation.js
@@ -1,55 +1,88 @@
 (function() {
 
     firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
+        if (user) {
 
-      } else {
-        // No user is signed in.
-        alert("Logging out!");
-        window.location.href = "login.html";
-      }
+        } else {
+            // No user is signed in.
+            alert("Logging out!");
+            window.location.href = "login.html";
+        }
     });
 
     var groupNum = 1;
 
     $(document).ready(function() {
-        
+
         populateCheckBox();
 
+
+        $('#announcementEntry_form').bootstrapValidator({
+            container: '#messages',
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            fields: {
+                announcement_title: {
+                    validators: {
+                        notEmpty: {
+                            message: 'The title is required and cannot be empty'
+                        }
+                    }
+                },
+                announcement_message: {
+                    validators: {
+                        notEmpty: {
+                            message: 'An Announcement is required and cannot be empty'
+                        }
+
+                    }
+                },
+                'group[]': {
+                    validators: {
+                        choice: {
+                            min: 1,
+                            max: 4,
+                            message: 'Please choose at least one Distribution Group'
+                        }
+                    }
+
+                }
+            }
+
+        });
+
+
+
         //when the back button is clicked, go back to announcements page    
-        $("#back_button").on("click", function(){
+        $("#back_button").on("click", function() {
             window.location.href = "announcements.html";
-            // var groups = [];
-            // $(".checkboxGroups").each(function(){
-            //     if($(this).is(':checked')){  
-            //         console.log($(this).attr('data-group'));
-            //         groups.push($(this).attr('data-group'));
-            //     }
-               
-            // });
-            // console.log( groups);
         });
 
         //when the submit button is pressed for the new announcements form,
         //get the data and try to add new announcement to firebase
-        $("#announcementEntry_form").on("submit", function(e){
-            e.preventDefault();
-            var priority = $("#announcemet_priority").val();
-            var title = $("#announcement_title").val();
-            var message = $("#announcement_message").val();
-            var user = new User(null, "Jeffery", "Calhoun", "jcd39@mail.umsl.edu", null, true, true, null, null);
-            
-            //add checked boxes to groups array
-            var groups = [];
-            $(".checkboxGroups").each(function(){
-                if($(this).is(':checked')){  
-                    groups.push($(this).attr('data-group'));
-                }
-            });
-            
-            addAnnouncement(user, title, message, priority, groups);
+        $("#announcementEntry_form").on("submit", function(e) {
+            if (!e.isDefaultPrevented()) {
+                e.preventDefault();
+                var priority = $("#announcemet_priority").val();
+                var title = $("#announcement_title").val();
+                var message = $("#announcement_message").val();
+                var user = new User(null, "Jeffery", "Calhoun", "jcd39@mail.umsl.edu", null, true, true, null, null);
+
+                //add checked boxes to groups array
+                var groups = [];
+                $(".checkboxGroups").each(function() {
+                    if ($(this).is(':checked')) {
+                        groups.push($(this).attr('data-group'));
+                    }
+                });
+                console.log("success");
+                addAnnouncement(user, title, message, priority, groups);
+            }
         });
-  
+
     });
 
 
@@ -60,8 +93,9 @@
 
         //query firebase group nodes and use 'name' to populate checkbox group
         groupRef.orderByChild('name').on('child_added', function(snapshot) {
-           
+
             $("#groupCheckbox").append(groupHtmlFromObject(snapshot.val()));
+            $('#announcementEntry_form').bootstrapValidator('addField', 'group[]');
             groupNum++;
         });
 
@@ -70,9 +104,9 @@
     //create checkbox input dynamically and use 'data-group' attribute to store group name for DB record downstream
     function groupHtmlFromObject(fbGroup) {
 
-        var html = '<input type = "checkbox" id = "group' + groupNum + '"class = "checkboxGroups" name = "group' + groupNum + '"';
+        var html = '<input type = "checkbox" id = "group' + groupNum + '"class = "checkboxGroups" name = "group[]"';
         html += 'data-group="' + fbGroup.name + '">';
-        html +='<label>' + fbGroup.name + '</label></br>';
+        html += '<label>' + fbGroup.name + '</label></br>';
         return html;
     }
 
@@ -96,10 +130,10 @@
         firebase.database().ref().update(updates);
 
     }
-    
+
 
     //Creates a new announcement in Firebase
-    function addAnnouncement(faculty, title, message, priority, groups){
+    function addAnnouncement(faculty, title, message, priority, groups) {
 
         var announcementsRef = firebase.database().ref('announcements/');
         var newAnnouncementKey = announcementsRef.push().key;
@@ -107,9 +141,9 @@
 
         var updates = {};
         updates['/announcements/' + newAnnouncementKey] = newAnnouncement;
-        firebase.database().ref().update(updates).then(function(){
-            window.location.href="announcements.html";
-        }, function(error){
+        firebase.database().ref().update(updates).then(function() {
+            window.location.href = "announcements.html";
+        }, function(error) {
             alert("Failed to add announcement!");
         });
     }
