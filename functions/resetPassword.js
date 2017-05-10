590aabc3a0cacde4aa56f3c047e34bd506055daf
thetotaljim <thetotaljim@users.noreply.github.com>
2017-05-09 20:39:12 -0500

    Update resetPassword.js

--- functions/resetPassword.js
+++ functions/resetPassword.js
@@ -1,3 +1,16 @@
+/*
+ *  CS 4500 Introduction to the Software Engineering Profession
+ *  Keith W. Miller
+ *  UMSL Music Department Announcement Application
+ *  Code Monkey Mafia
+ *  Amanda Rawls - Group Leader
+ *  Jeffery Calhoun
+ *  Stefan Rothermich
+ *  James Steimel
+ *  
+ *  Javascript to reset passwords.
+ */
+
 (function(){
 	$(document).ready(function(){
 		
